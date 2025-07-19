8734e64b6b01bea440aac716b8b83f72b0528a62
Alec Fessler <afessler1998@gmail.com>
2025-07-18 21:37:27 -0500

    disabled some cpu features in build.zig since the kernel does not currently enable them yet, ie sse

--- build.zig
+++ build.zig
@@ -14,10 +14,22 @@ pub fn build(b: *std.Build) void {
     bootstrap_obj.addFileArg(b.path("kernel/arch/x86_64/bootstrap.asm"));
     bootstrap_obj.step.dependOn(&mkdir_obj.step);
 
+    var disabled_features = std.Target.Cpu.Feature.Set.empty;
+    var enabled_features = std.Target.Cpu.Feature.Set.empty;
+
+    disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.mmx));
+    disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.sse));
+    disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.sse2));
+    disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.avx));
+    disabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.avx2));
+    enabled_features.addFeature(@intFromEnum(std.Target.x86.Feature.soft_float));
+
     const target = b.resolveTargetQuery(.{
         .cpu_arch = std.Target.Cpu.Arch.x86_64,
         .os_tag = std.Target.Os.Tag.freestanding,
         .abi = std.Target.Abi.none,
+        .cpu_features_sub = disabled_features,
+        .cpu_features_add = enabled_features,
     });
     const optimize = b.standardOptimizeOption(.{});
 
