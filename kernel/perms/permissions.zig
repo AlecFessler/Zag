90b382de86681704c17a120d55ea7e73819a9b6d
Alec Fessler <afessler1998@gmail.com>
2026-03-21 23:49:12 -0500

    perms/ spec compliance updates

--- kernel/perms/permissions.zig
+++ kernel/perms/permissions.zig
@@ -1,8 +1,9 @@
 const zag = @import("zag");
 
+const DeviceRegion = zag.memory.device_region.DeviceRegion;
 const Process = zag.sched.process.Process;
-const VmReservation = zag.memory.vmm.VmReservation;
 const SharedMemory = zag.memory.shared.SharedMemory;
+const VAddr = zag.memory.address.VAddr;
 
 pub const ProcessRights = packed struct(u8) {
     grant_to: bool = false,
@@ -18,7 +19,9 @@ pub const VmReservationRights = packed struct(u8) {
     read: bool = false,
     write: bool = false,
     execute: bool = false,
-    _reserved: u5 = 0,
+    shareable: bool = false,
+    mmio: bool = false,
+    _reserved: u3 = 0,
 };
 
 pub const SharedMemoryRights = packed struct(u8) {
@@ -29,24 +32,45 @@ pub const SharedMemoryRights = packed struct(u8) {
     _reserved: u4 = 0,
 };
 
+pub const DeviceRegionRights = packed struct(u8) {
+    read: bool = false,
+    write: bool = false,
+    grant: bool = false,
+    _reserved: u5 = 0,
+};
+
 pub const PermissionEntry = struct {
+    handle: u64,
     object: KernelObject,
     rights: u8,
 
     pub fn processRights(self: @This()) ProcessRights {
         return @bitCast(self.rights);
     }
+
     pub fn vmRights(self: @This()) VmReservationRights {
         return @bitCast(self.rights);
     }
+
     pub fn shmRights(self: @This()) SharedMemoryRights {
+        return @bitCast(self.rights);
+    }
+
+    pub fn deviceRights(self: @This()) DeviceRegionRights {
         return @bitCast(self.rights);
     }
 };
 
+pub const VmReservationObject = struct {
+    max_rights: VmReservationRights,
+    original_start: VAddr,
+    original_size: u64,
+};
+
 pub const KernelObject = union(enum) {
     process: *Process,
-    vm_reservation: *VmReservation,
+    vm_reservation: VmReservationObject,
     shared_memory: *SharedMemory,
+    device_region: *DeviceRegion,
     empty: void,
 };
